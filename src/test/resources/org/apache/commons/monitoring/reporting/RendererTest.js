cbabcf026c8be0466e3b9d64a0e61ac553092e07
Nicolas De Loof <nicolas@apache.org>
2008-01-30 19:14:30 +0000

    fix test
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/monitoring/trunk@616849 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/resources/org/apache/commons/monitoring/reporting/RendererTest.js
+++ src/test/resources/org/apache/commons/monitoring/reporting/RendererTest.js
@@ -1,6 +1,6 @@
 [{key:{name:"JsonRendererTest.setUp",category:"test",subsystem:"reporting"},
 concurrency:{value:"0",min:"0",max:"0",mean:"NaN",stdDev:"NaN"},
-performances:{value:"10",min:"10",max:"10",mean:"10.0",stdDev:"NaN",total:"0",hits:"1"}},
+performances:{value:"10",min:"10",max:"10",mean:"10.0",stdDev:"NaN",total:"10",hits:"1"}},
 {key:{name:"TestCase",category:"test",subsystem:"junit"},
 concurrency:{value:"1",min:"1",max:"1",mean:"0.0",stdDev:"NaN"},
 performances:{value:"0",min:"0",max:"0",mean:"NaN",stdDev:"NaN",total:"0",hits:"0"}}
