b7e558073050d2fa92ef8d7fbbb96cd2561a1eef
Romain Manni-Bucau <rmannibucau@apache.org>
2013-11-15 16:15:14 +0000

    adding abstract CounterGauge to be able to write custom gauge on counters quickly
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sirona/trunk@1542306 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/sirona/gauges/counter/CounterGauge.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sirona.gauges.counter;
+
+import org.apache.sirona.Role;
+import org.apache.sirona.configuration.Configuration;
+import org.apache.sirona.counters.Counter;
+import org.apache.sirona.gauges.Gauge;
+
+public abstract class CounterGauge implements Gauge {
+    private final Counter counter;
+    private final boolean reset;
+
+    protected CounterGauge(final Counter counter) {
+        this(counter, true);
+    }
+
+    protected CounterGauge(final Counter counter, final boolean reset) {
+        this.counter = counter;
+        this.reset = reset;
+    }
+
+    @Override
+    public Role role() {
+        return counter.getKey().getRole();
+    }
+
+    @Override
+    public double value() {
+        try {
+            return counter.getSum();
+        } finally {
+            if (reset) {
+                counter.reset();
+            }
+        }
+    }
+
+    @Override
+    public long period() {
+        return Configuration.getInteger(Configuration.CONFIG_PROPERTY_PREFIX + "gauge.counter.period", 4000);
+    }
+}
