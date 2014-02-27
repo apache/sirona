b6dcfb05b996b88a9b1e7e6cdd1329e08dad63db
Olivier Lamy <olamy@apache.org>
2014-02-27 02:58:47 +0000

    better with adding test class :-)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sirona/trunk@1572416 13f79535-47bb-0310-9956-ffa450edef68

---
+++ agent/javaagent/src/test/java/org/apache/test/sirona/javaagent/PathTrackingInvocationRecursiveTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.test.sirona.javaagent;
+
+import org.apache.sirona.configuration.ioc.IoCs;
+import org.apache.sirona.javaagent.AgentArgs;
+import org.apache.sirona.javaagent.JavaAgentRunner;
+import org.apache.sirona.pathtracking.test.ExtendedInMemoryPathTrackingDataStore;
+import org.apache.sirona.store.DataStoreFactory;
+import org.apache.sirona.tracking.PathTrackingEntry;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * this test validate we don't in StackOverflow when redirecting System.out and using debug mode
+ */
+@RunWith(JavaAgentRunner.class)
+public class PathTrackingInvocationRecursiveTest
+{
+
+    @Test
+    @AgentArgs(value = "",
+               sysProps = "project.build.directory=${project.build.directory}|sirona.agent.debug=true|org.apache.sirona.configuration.sirona.properties=${project.build.directory}/test-classes/pathtracking/sirona.properties")
+    public void simpleTest()
+        throws Exception
+    {
+
+        App app = new App().redirectStreamout();
+        app.beer();
+
+        DataStoreFactory dataStoreFactory = IoCs.findOrCreateInstance( DataStoreFactory.class );
+
+        ExtendedInMemoryPathTrackingDataStore ptds =
+            ExtendedInMemoryPathTrackingDataStore.class.cast( dataStoreFactory.getPathTrackingDataStore() );
+
+        Map<String, Set<PathTrackingEntry>> all = ptds.retrieveAll();
+
+        System.out.println( all );
+
+        Assert.assertTrue( !all.isEmpty() );
+
+
+        Assert.assertEquals( 4, all.size() );
+
+    }
+
+}
