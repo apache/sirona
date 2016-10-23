c7e8d51dd3a9fe1130eead723b6c72268034896e
Romain Manni-Bucau <rmannibucau@apache.org>
2016-10-23 09:22:36 +0000

    better with EOL
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sirona/trunk@1766256 13f79535-47bb-0310-9956-ffa450edef68

--- agent/javaagent/src/main/java/org/apache/sirona/javaagent/SironaAgent.java
+++ agent/javaagent/src/main/java/org/apache/sirona/javaagent/SironaAgent.java
@@ -90,10 +90,10 @@ public class SironaAgent {
                     FileWriter writer = null;
                     try {
                         writer = new FileWriter(dumpOnExit);
-                        writer.write("name;role;unit;average;min;max;hits;max concurrency");
+                        writer.write("name;role;unit;average;min;max;hits;max concurrency\n");
                         for (final Counter c : Repository.INSTANCE.counters()) {
                             writer.write(c.getKey().getName() + ";" + c.getKey().getRole().getName() + ";" + c.getKey().getRole().getUnit().getName()
-                                    + ";" + c.getMean() + ";" + c.getMin() + ";" + c.getMax() + ";" + c.getHits() + ";" + c.getMaxConcurrency());
+                                    + ";" + c.getMean() + ";" + c.getMin() + ";" + c.getMax() + ";" + c.getHits() + ";" + c.getMaxConcurrency() + "\n");
                         }
                     } catch (final IOException e) {
                         throw new IllegalStateException(e);
